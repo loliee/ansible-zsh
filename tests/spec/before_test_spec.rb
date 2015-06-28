c8548e175a124929fc39096dbd369547eb5a6f97
Maxime Loliée <maxime@siliadev.com>
2015-06-28 18:08:12 +0200

    fix: Try to fix travis build ... :(

--- tests/spec/before_test_spec.rb
+++ tests/spec/before_test_spec.rb
@@ -14,10 +14,6 @@ describe file('/home/mtester/.zfunctions/') do
   it { should_not be_directory }
 end
 
-describe command('/home/mtester/.zfunctions') do
-  it { should_not be_file }
-end
-
 describe file('/home/mtester/.zfunctions/prompt_mlpure_setup') do
   it { should_not be_file }
 end
