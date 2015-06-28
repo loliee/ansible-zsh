b5834e5824e4b64b523364d1fe7fc265733b3cf8
Maxime Loliée <maxime@siliadev.com>
2015-06-28 18:01:16 +0200

    fix: Try to fix travis build ... :(

--- tests/spec/configure_spec.rb
+++ tests/spec/configure_spec.rb
@@ -20,7 +20,7 @@ end
 
 describe file('/home/mtester/.zfunctions/.zshrc') do
   its(:content) { should contain /# Ansible managed:/ }
-  its(:content) { should contain /fpath=("$HOME/.zfunctions" $fpath)/ }
+  its(:content) { should contain /fpath=\("$HOME/.zfunctions" $fpath\)/ }
   its(:content) { should contain /autoload -U promptinit && promptinit/ }
   its(:content) { should contain /prompt mlpure/ }
   its(:content) { should contain /alias ll='ls -la'/ }
