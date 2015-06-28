7fa899edfd252286a064ec14098e0dcfabfa9385
Maxime Loliée <maxime@siliadev.com>
2015-06-28 17:28:44 +0200

    fix: Try to fix travis build ... :(

---
+++ tests/spec/before_test_spec.rb
@@ -0,0 +1,39 @@
+require 'serverspec'
+
+set :backend, :exec
+
+describe package('zsh') do
+  it { should_not be_installed }
+end
+
+describe command('/home/mtester/.zshrc') do
+  it { should_not be_file }
+end
+
+describe file('/home/mtester/.zfunctions/') do
+  it { should_not be_directory }
+end
+
+describe command('/home/mtester/.zfunctions') do
+  it { should_not be_file }
+end
+
+describe file('/home/mtester/.zfunctions/prompt_mlpure_setup') do
+  it { should_not be_file }
+end
+
+describe file('/home/mtester/.zfunctions/async') do
+  it { should_not be_file }
+end
+
+describe file('/home/mtester/.zfunctions/prompt_mlpure_setup') do
+  it { should_not be_file }
+end
+
+describe user('mtester') do
+  it { should have_home_directory '/home/mtester' }
+end
+
+describe user('mtester') do
+  it { should have_login_shell '/bin/bash' }
+end
